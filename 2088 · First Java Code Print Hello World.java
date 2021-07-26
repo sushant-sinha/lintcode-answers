0a9bf37f7d15f781aedb6b98db0cf6a9c698c9f4
sushant-sinha <sinhasushant04@gmail.com>
2021-07-26 08:41:26 +0530

    testing

---
+++ 2088 · First Java Code Print Hello World.java
@@ -0,0 +1,16 @@
+// SUSHANT SINHA
+
+// 101ms 8.92mb ( 100% )
+
+public class HelloWorld {
+    /* Your first java code
+     * print Hello World to console
+     */
+    public static void main(String[] args) {
+        // write your code here
+        // use System.out.println to print Hello World to console.
+        
+        System.out.println("Hello World");
+
+    }
+}
