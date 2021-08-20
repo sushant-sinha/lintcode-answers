0355dd9b322144375021b8e54ae9ce5f38590b26
sushant-sinha <sinhasushant04@gmail.com>
2021-08-20 16:57:36 +0530

    new addition

---
+++ 1428 · Keys and Rooms.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param rooms: a list of keys rooms[i]
+     * @return: can you enter every room
+     */
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        // Write your code here
+        Set<Integer> opened = new HashSet();
+        Queue<Integer> queue = new LinkedList();
+        opened.add(0);
+        queue.add(0);
+        
+        while(!queue.isEmpty()) {
+            int index = queue.poll();
+            for (int key : rooms.get(index)) {
+                if (!opened.contains(key)) {
+                    opened.add(key);
+                    queue.add(key);
+                }
+            }
+        }
+        return opened.size() == rooms.size();
+    }
+}
