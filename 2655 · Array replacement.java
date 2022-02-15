138317fe713a4e99537457d3957d79558f05b46e
sushant-sinha <sinhasushant04@gmail.com>
2022-02-15 21:04:34 +0530

    new addition

---
+++ 2655 · Array replacement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int[] replacement(int[] arr1, int[] arr2) {
+        // -- write your code here --
+
+        int index=arr1.length-arr2.length;
+
+        for(int i:arr2){
+            arr1[index++]=i;
+        }
+
+        return arr1;
+		
+    }
+}
