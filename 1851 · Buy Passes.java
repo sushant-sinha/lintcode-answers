b704dbc8c93a0d9b6953217d908e5ad23d27fe1a
sushant-sinha <sinhasushant04@gmail.com>
2022-02-16 20:17:00 +0530

    new addition

---
+++ 1851 · Buy Passes.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     * @param arr: the line 
+     * @param k: Alex place
+     * @return: the time when Alex requires to buy all passes
+     */
+    public int buyPasses(int[] arr, int k) {
+        // Write your code here.
+
+        int ans = 0;
+
+        while (arr[k] != 0) {
+
+            for (int index = 0; index < arr.length; index++) {
+
+                if (arr[index] > 0) {
+                    ans++;
+                    arr[index]--;
+                    if (arr[k] == 0)
+                        return ans;
+                }
+            }
+
+        }
+
+        return ans;
+    }
+}
